23233d786670cba1e47d7d8855c41fc2e9b853da
Arturo Iafrate <arturo.iafrate@gmail.com>
2017-11-15 16:45:26 +0100

    - input files (txt files in .m files) ported to json. - Added a JSON parser to parse input files (depends on JSON.simple v1.1.1 library).

---
+++ src/it/uniparthenope/MyJSONParser.java
@@ -0,0 +1,39 @@
+package it.uniparthenope;
+
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class MyJSONParser {
+    private JSONObject object;
+
+    public MyJSONParser(String path){
+        JSONParser parser = new JSONParser();
+        try{
+            Object obj = parser.parse(new FileReader("inputFiles/"+path));
+            object = (JSONObject) obj;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getValueAsString(String key){
+        String value = (String) object.get(key);
+        if(value == null){
+            value = "NOT FOUND";
+        }
+        return value;
+    }
+
+    public int getValueAsInt(String key){
+        int value = (int) object.get(key);
+        return value;
+    }
+
+    public double getValueAsDouble(String key){
+        double value = (double) object.get(key);
+        return value;
+    }
+}
