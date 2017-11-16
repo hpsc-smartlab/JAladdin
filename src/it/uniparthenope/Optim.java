c05a80335f55ff331c7641a36df88f04444b0c54
Arturo Iafrate <arturo.iafrate@gmail.com>
2017-11-16 09:55:31 +0100

    Optimal parameters

--- src/it/uniparthenope/Optim.java
+++ src/it/uniparthenope/Optim.java
@@ -1,17 +1,39 @@
 package it.uniparthenope;
 
 public class Optim {
+    //from settings.m
     private int relocAnlsCode;//code of the "analysis" of the  relocatable wave model
+
+    private int intentional_speed_red;
+    private int windModel;
+    private int waveModel;
 
     public Optim(){
         this.relocAnlsCode = 20;
+    }
+
+    public void OptimizationParameters(){
+        //getting data from optim_pars.json parsing
+        MyJSONParser parser = new MyJSONParser("optim_pars.json");
+        this.intentional_speed_red = parser.getValueAsInt("intentional_speed_red");
+        this.windModel = parser.getValueAsInt("windModel");
+        this.waveModel = parser.getValueAsInt("waveModel");
     }
 
     public int getRelocAnlsCode() {
         return relocAnlsCode;
     }
 
-    public void setRelocAnlsCode(int relocAnlsCode) {
-        this.relocAnlsCode = relocAnlsCode;
+    public int getIntentional_speed_red() {
+        return intentional_speed_red;
     }
+
+    public int getWindModel() {
+        return windModel;
+    }
+
+    public int getWaveModel() {
+        return waveModel;
+    }
+
 }
