7e263f2c55b0b4cbd6ba98e4d4b424342ed9297d
Arturo Iafrate <arturo.iafrate@gmail.com>
2017-11-23 15:46:16 +0100

    added some stuff

--- src/it/uniparthenope/SpatialGrid.java
+++ src/it/uniparthenope/SpatialGrid.java
@@ -15,6 +15,20 @@ public class SpatialGrid {
     private double bbox__lon_min;
     private double bbox__lat_min;
     private double bbox__lon_max;
+    private double DB_xi;
+    private double DB_yi;
+    private double DB_xf;
+    private double DB_yf;
+    private long DB_Nx;
+    private long DB_Ny;
+    private double xi;
+    private double yi;
+    private double xf;
+    private double yf;
+    private double inset_area;
+    private long inset_Nx;
+    private long inset_Ny;
+
 
     public SpatialGrid(){
         this.numberOfNeighbors = 24;
@@ -120,5 +134,109 @@ public class SpatialGrid {
 
     public double getInv_step(){
         return  this.inv_step;
+    }
+
+    public double getDB_xi() {
+        return DB_xi;
+    }
+
+    public void setDB_xi(double DB_xi) {
+        this.DB_xi = DB_xi;
+    }
+
+    public double getDB_yi() {
+        return DB_yi;
+    }
+
+    public void setDB_yi(double DB_yi) {
+        this.DB_yi = DB_yi;
+    }
+
+    public double getDB_xf() {
+        return DB_xf;
+    }
+
+    public void setDB_xf(double DB_xf) {
+        this.DB_xf = DB_xf;
+    }
+
+    public double getDB_yf() {
+        return DB_yf;
+    }
+
+    public void setDB_yf(double DB_yf) {
+        this.DB_yf = DB_yf;
+    }
+
+    public long getDB_Nx() {
+        return DB_Nx;
+    }
+
+    public void setDB_Nx(long DB_Nx) {
+        this.DB_Nx = DB_Nx;
+    }
+
+    public long getDB_Ny() {
+        return DB_Ny;
+    }
+
+    public void setDB_Ny(long DB_Ny) {
+        this.DB_Ny = DB_Ny;
+    }
+
+    public double getXi() {
+        return xi;
+    }
+
+    public void setXi(double xi) {
+        this.xi = xi;
+    }
+
+    public double getYi() {
+        return yi;
+    }
+
+    public void setYi(double yi) {
+        this.yi = yi;
+    }
+
+    public double getXf() {
+        return xf;
+    }
+
+    public void setXf(double xf) {
+        this.xf = xf;
+    }
+
+    public double getYf() {
+        return yf;
+    }
+
+    public void setYf(double yf) {
+        this.yf = yf;
+    }
+
+    public double getInset_area() {
+        return inset_area;
+    }
+
+    public void setInset_area(double inset_area) {
+        this.inset_area = inset_area;
+    }
+
+    public long getInset_Nx() {
+        return inset_Nx;
+    }
+
+    public void setInset_Nx(long inset_Nx) {
+        this.inset_Nx = inset_Nx;
+    }
+
+    public long getInset_Ny() {
+        return inset_Ny;
+    }
+
+    public void setInset_Ny(long inset_Ny) {
+        this.inset_Ny = inset_Ny;
     }
 }
