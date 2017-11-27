fdbbc87dcc3f5f71ed725be178854801d718c5ad
Arturo Iafrate <arturo.iafrate@gmail.com>
2017-11-27 12:35:20 +0100

    Binary files parser

---
+++ src/it/uniparthenope/MyBinaryParser.java
@@ -0,0 +1,34 @@
+package it.uniparthenope;
+
+import java.io.*;
+
+public class MyBinaryParser {
+    private String fileName;
+    private File file;
+    private DataInputStream din;
+
+    public MyBinaryParser(String fileName){
+        this.fileName = fileName;
+        try{
+            this.file = new File(fileName);
+            FileInputStream fin = new FileInputStream(this.file);
+            BufferedInputStream bin = new BufferedInputStream(fin);
+            this.din = new DataInputStream(bin);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public long[] readAsUint32(){
+        int count = (int) file.length()/4;
+        long[] values = new long[count];
+        try{
+            for(int i=0;i<count;i++) {
+                values[i] = (long) this.din.readInt();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return values;
+    }
+}
