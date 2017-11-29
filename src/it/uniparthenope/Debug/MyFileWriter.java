479bb16375c03c85e1c9cd69f81ebc54b1ad3e12
Arturo Iafrate <arturo.iafrate@gmail.com>
2017-11-29 19:47:33 +0100

    Added some method to speed up debugging

--- src/it/uniparthenope/Debug/MyFileWriter.java
+++ src/it/uniparthenope/Debug/MyFileWriter.java
@@ -3,6 +3,7 @@ package it.uniparthenope.Debug;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MyFileWriter {
     private String fileName;
@@ -22,6 +23,93 @@ public class MyFileWriter {
             this.bw = new BufferedWriter(new FileWriter("DebugFiles/"+this.fileName+".txt",true));
         } catch (Exception e){
             e.printStackTrace();
+        }
+    }
+
+    public void WriteValue(String varName, Double value){
+        WriteLine(varName+": "+value);
+    }
+
+    public void WriteValue(String varName, Long value){
+        WriteLine(varName+": "+value);
+    }
+
+    public void WriteValue(String varName, Integer value){
+        WriteLine(varName+": "+value);
+    }
+
+    public void WriteDoubleArray(String varName, ArrayList<Double> array){
+        WriteLine(varName+":");
+        for(Double element: array){
+            WriteLine(element.toString());
+        }
+    }
+
+    public void WriteLongArray(String varName, ArrayList<Long> array){
+        WriteLine(varName+":");
+        for(Long element: array){
+            WriteLine(element.toString());
+        }
+    }
+
+    public void WriteIntegerArray(String varName, ArrayList<Integer> array){
+        WriteLine(varName+":");
+        for(Integer element: array){
+            WriteLine(element.toString());
+        }
+    }
+
+    public void WriteDoubleArray(String varName, Double[] array){
+        WriteLine(varName+":");
+        for(int i=0;i<array.length;i++){
+            WriteLine(array[i].toString());
+        }
+    }
+
+    public void WriteLongArray(String varName, Long[] array){
+        WriteLine(varName+":");
+        for(int i=0;i<array.length;i++){
+            WriteLine(array[i].toString());
+        }
+    }
+
+    public void WriteIntegerArray(String varName, Integer[] array){
+        WriteLine(varName+":");
+        for(int i=0;i<array.length;i++){
+            WriteLine(array[i].toString());
+        }
+    }
+
+    public void WriteDoubleMatrix(String varName, Double[][] matrix){
+        WriteLine(varName+":");
+        for(int i=0;i<matrix.length;i++){
+            String row="";
+            for(int j=0;j<matrix[0].length;j++){
+                row+=matrix[i][j]+" ";
+            }
+            WriteLine(row);
+        }
+    }
+
+    public void WriteIntMatrix(String varName, Integer[][] matrix){
+        WriteLine(varName+":");
+        for(int i=0;i<matrix.length;i++){
+            String row="";
+            for(int j=0;j<matrix[0].length;j++){
+                row+=matrix[i][j]+" ";
+            }
+            WriteLine(row);
+        }
+    }
+
+    public void WriteLongMatrix(String varName, Long[][] matrix){
+        WriteLine(varName+":");
+        for(int i=0;i<matrix.length;i++){
+            String row="";
+            for(int j=0;j<matrix[0].length;j++){
+                row+=matrix[i][j]+" ";
+            }
+            WriteLine(row);
         }
     }
 
