7a9f3980b58f058c3959acaac9d613a0790c62a4
Arturo Iafrate <arturo.iafrate@gmail.com>
2017-12-05 16:46:11 +0100

    deg2utm results class

---
+++ src/it/uniparthenope/Boxing/deg2utmResults.java
@@ -0,0 +1,25 @@
+package it.uniparthenope.Boxing;
+
+public class deg2utmResults {
+    private double[] x;
+    private double[] y;
+    private String[] utmzone;
+
+    public deg2utmResults(double[] x, double[] y, String[] utmzone){
+        this.x = x;
+        this.y = y;
+        this.utmzone = utmzone;
+    }
+
+    public double[] getX() {
+        return x;
+    }
+
+    public double[] getY() {
+        return y;
+    }
+
+    public String[] getUtmzone() {
+        return utmzone;
+    }
+}
