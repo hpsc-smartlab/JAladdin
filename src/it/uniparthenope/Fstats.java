378699b13968abddfb7babc6c12322f750c1cbd6
Arturo Iafrate <arturo.iafrate@gmail.com>
2017-12-06 16:37:31 +0100

    Fstats class.

---
+++ src/it/uniparthenope/Fstats.java
@@ -0,0 +1,22 @@
+package it.uniparthenope;
+
+public class Fstats {
+    private double wheight_min;
+    private double wheight_max;
+
+    public double getWheight_min() {
+        return wheight_min;
+    }
+
+    public void setWheight_min(double wheight_min) {
+        this.wheight_min = wheight_min;
+    }
+
+    public double getWheight_max() {
+        return wheight_max;
+    }
+
+    public void setWheight_max(double wheight_max) {
+        this.wheight_max = wheight_max;
+    }
+}
