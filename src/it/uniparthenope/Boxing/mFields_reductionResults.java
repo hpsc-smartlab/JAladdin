de757441abbd335473c7e767080c798f335afebc
Arturo Iafrate <arturo.iafrate@gmail.com>
2017-12-11 17:25:17 +0100

    mFields_reduction and fieldStats results boxing classes

---
+++ src/it/uniparthenope/Boxing/mFields_reductionResults.java
@@ -0,0 +1,49 @@
+package it.uniparthenope.Boxing;
+
+public class mFields_reductionResults {
+    private double[] lat_red;
+    private double[] lon_red;
+    private double[][][] out1;
+    private double[][][] out2;
+    private double[][][] out3;
+
+    public double[] getLat_red() {
+        return lat_red;
+    }
+
+    public void setLat_red(double[] lat_red) {
+        this.lat_red = lat_red;
+    }
+
+    public double[] getLon_red() {
+        return lon_red;
+    }
+
+    public void setLon_red(double[] lon_red) {
+        this.lon_red = lon_red;
+    }
+
+    public double[][][] getOut1() {
+        return out1;
+    }
+
+    public void setOut1(double[][][] out1) {
+        this.out1 = out1;
+    }
+
+    public double[][][] getOut2() {
+        return out2;
+    }
+
+    public void setOut2(double[][][] out2) {
+        this.out2 = out2;
+    }
+
+    public double[][][] getOut3() {
+        return out3;
+    }
+
+    public void setOut3(double[][][] out3) {
+        this.out3 = out3;
+    }
+}
