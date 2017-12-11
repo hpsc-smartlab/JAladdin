bbb5d884ac538894457b27f9079ea365d8bcbae5
Arturo Iafrate <arturo.iafrate@gmail.com>
2017-12-11 17:24:32 +0100

    Added some attributes and (getter/setter) methods.

--- src/it/uniparthenope/Fstats.java
+++ src/it/uniparthenope/Fstats.java
@@ -3,6 +3,13 @@ package it.uniparthenope;
 public class Fstats {
     private double wheight_min;
     private double wheight_max;
+    private double bathy_min;
+    private double bathy_max;
+    private double wperiod_min;
+    private double wperiod_max;
+    private double[][] wheight_avg;
+    private double wlength_min;
+    private double wlenght_max;
 
     public double getWheight_min() {
         return wheight_min;
@@ -18,5 +25,61 @@ public class Fstats {
 
     public void setWheight_max(double wheight_max) {
         this.wheight_max = wheight_max;
+    }
+
+    public double getBathy_min() {
+        return bathy_min;
+    }
+
+    public void setBathy_min(double bathy_min) {
+        this.bathy_min = bathy_min;
+    }
+
+    public double getBathy_max() {
+        return bathy_max;
+    }
+
+    public void setBathy_max(double bathy_max) {
+        this.bathy_max = bathy_max;
+    }
+
+    public double[][] getWheight_avg() {
+        return wheight_avg;
+    }
+
+    public void setWheight_avg(double[][] wheight_avg) {
+        this.wheight_avg = wheight_avg;
+    }
+
+    public double getWperiod_min() {
+        return wperiod_min;
+    }
+
+    public void setWperiod_min(double wperiod_min) {
+        this.wperiod_min = wperiod_min;
+    }
+
+    public double getWperiod_max() {
+        return wperiod_max;
+    }
+
+    public void setWperiod_max(double wperiod_max) {
+        this.wperiod_max = wperiod_max;
+    }
+
+    public double getWlength_min() {
+        return wlength_min;
+    }
+
+    public void setWlength_min(double wlength_min) {
+        this.wlength_min = wlength_min;
+    }
+
+    public double getWlenght_max() {
+        return wlenght_max;
+    }
+
+    public void setWlenght_max(double wlenght_max) {
+        this.wlenght_max = wlenght_max;
     }
 }
