070f5ba0da7fac543328871dcfe3c7a519c70fe7
arturoiafrate <arturo.iafrate@gmail.com>
2018-01-08 16:15:22 +0100

    polar_twa and polar_tws switched from double to ArrayList<Double>

--- src/it/uniparthenope/Boxing/vessel_ResponseResults.java
+++ src/it/uniparthenope/Boxing/vessel_ResponseResults.java
@@ -1,14 +1,15 @@
 package it.uniparthenope.Boxing;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class vessel_ResponseResults {
+public class vessel_ResponseResults implements Serializable {
     private double[][] ship_v_LUT;
     private ArrayList<Double> H_array_m;
-    private double polar_twa;
-    private double polar_tws;
+    private ArrayList<Double> polar_twa;
+    private ArrayList<Double> polar_tws;
 
-    public vessel_ResponseResults(double[][] ship_v_LUT, ArrayList<Double> H_array_m, double polar_twa, double polar_tws){
+    public vessel_ResponseResults(double[][] ship_v_LUT, ArrayList<Double> H_array_m, ArrayList<Double> polar_twa, ArrayList<Double> polar_tws){
         this.ship_v_LUT = ship_v_LUT;
         this.H_array_m = H_array_m;
         this.polar_twa = polar_twa;
@@ -26,11 +27,11 @@ public class vessel_ResponseResults {
         return H_array_m;
     }
 
-    public double getPolar_twa() {
+    public ArrayList<Double> getPolar_twa() {
         return polar_twa;
     }
 
-    public double getPolar_tws() {
+    public ArrayList<Double> getPolar_tws() {
         return polar_tws;
     }
 }
